f04ba3b8a1f2a6a8f6d8288854546e35367f86d8
Luna Rae <kra7888@gmail.com>
2023-02-05 22:31:54 -0500

    added shape js files and cli file, updated file structure, added code to new js files

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-const inquirer = require('inquirer');
+const CLI = require('./lib/cli.js');
 
-const questions = [{
+const cli = new CLI();
 
-    type: "input",
-    message: "Please enter no more than three letters to be used in your logo",
-    name: "text"
-},
-{
-    type: "input",
-    message: "Enter a color of your text. This can be the color keyword or a hexadecimal number",
-    name: "text-color"
-},
-{
-    type: "list",
-    message: "Select a shape from the list below",
-    choices: [
-        "Circle",
-        "Triangle",
-        "Square",
-    ],
-
-    name: "shape"
-
-},
-
-{
-    type: "input",
-    message: "Enter the color of the shape. This can be the color keyword or a hexadecimal number",
-    name: "shape-color"
-}
-
-]
-
-inquirer.prompt(questions)
-    // .then(responseHandler);
+cli.run();
